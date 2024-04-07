822f45147ab549d499fb72eb15aedf5d0fcb7471
Nirdesh Mohan <NirdeshM@Gmail.com>
2024-04-06 23:18:10 -0400

    Update UrlShortner.java
    Cleanup unnecessary things

--- the-shortest-url-lqgygs/src/main/java/com/m/shorturl/util/UrlShortner.java
+++ the-shortest-url-lqgygs/src/main/java/com/m/shorturl/util/UrlShortner.java
@@ -7,49 +7,46 @@ import java.util.*;
 
 @Component
 public class UrlShortner {
-    //List to store the shortURL keys
-    private static final List<String> shortUrlKeyList = new ArrayList<>();
     //Map to store the shortURL key and actual LongURL
-    private static final Map<String,String> urlKeyMap = new HashMap<>();
+    private final Map<String,String> urlKeyMap = new HashMap<>();
     private final Random rand = new Random();
-    private final String short3MURL = "http://short3murl.com/";
     @Value("${short.url.max.length}")
-    private String shortUrlLen;
-
-    public void setShortUrlLen(String shortUrlLen) {
-        this.shortUrlLen = shortUrlLen;
-    }
-
-    public void setShortUrlKeyList(List<String>  shortUrlKeyList) {
-        UrlShortner.shortUrlKeyList.addAll(shortUrlKeyList);
-    }
-
-    public void setUrlKeyMap(Map<String,String> urlKeyMap) {
-        UrlShortner.urlKeyMap.putAll(urlKeyMap);
-    }
+    private int shortUrlLen;
 
     /** Encodes a URL to a shortened URL.
      * If short URL already exists then it regenerates the shortURL
      * @returns shortURL
      */
-    public String encode(String longUrl) {
+    public String encode(String url) {
         //Base64.getUrlEncoder().encodeToString(longUrl.getBytes());
-        int urlLength = Integer.parseInt(shortUrlLen);
+        //Base64.getUrlEncoder().encodeToString(longUrl.getBytes());
         String newShortURL;
-        char [] shortURLKey = new char[urlLength];
+        char [] shortURLKey = new char[shortUrlLen];
         String base64Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/";
 
-        for(int i = 0; i < urlLength; i++ )
+        for(int i = 0; i < shortUrlLen; i++ ) {
             shortURLKey[i] = base64Chars.charAt(rand.nextInt(base64Chars.length()));
-
-        if(!shortUrlKeyList.contains(String.copyValueOf(shortURLKey))){
-            newShortURL = short3MURL + String.copyValueOf(shortURLKey);
-            shortUrlKeyList.add(String.copyValueOf(shortURLKey));
-            urlKeyMap.put(String.copyValueOf(shortURLKey),longUrl);
-            return newShortURL;
-        }else{
-            encode(longUrl);
         }
+
+        if(urlKeyMap.containsValue(String.valueOf(shortURLKey))){
+            //In case key is already exists
+            encode(url);
+        }else{
+            String short3MURL = "http://short3murl.com/";
+            newShortURL = short3MURL + String.valueOf(shortURLKey);
+            urlKeyMap.put(String.valueOf(shortURLKey),url);
+            return newShortURL;
+        }
+
+//        if(!shortUrlKeyList.contains(String.copyValueOf(shortURLKey))){
+//            newShortURL = short3MURL + String.copyValueOf(shortURLKey);
+//            shortUrlKeyList.add(String.copyValueOf(shortURLKey));
+//            urlKeyMap.put(String.copyValueOf(shortURLKey),url);
+//            return newShortURL;
+//        }else{
+//            //In case key is already exists
+//            encode(url);
+//        }
 
         return null;
     }
@@ -58,8 +55,7 @@ public class UrlShortner {
      * If short URL already exists then it regenerates the shortURL
      * @returns shortURL
      */
-    public String decode(String shortUrl) {
-        int urlLen = Integer.parseInt(shortUrlLen);
-        return urlKeyMap.get(shortUrl.substring(shortUrl.length() - urlLen));
+    public String decode(String url) {
+        return urlKeyMap.get(url.substring(url.length() - shortUrlLen));
     }
 }
